10f456bd3e9c7a6782a8df66d08953210670dcee
Artur Prepelytsia <preart81@gmail.com>
2025-02-02 19:38:17 +0200

    update Sphinx configuration for improved documentation and add autodoc extension

--- docs/conf.py
+++ docs/conf.py
@@ -1,27 +1,29 @@
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+# <https://www.sphinx-doc.org/en/master/usage/configuration.html>
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+# <https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information>
+import sys
+import os
 
-project = 'Rest API Contacts Manager'
-copyright = '2025, PreArt'
-author = 'PreArt'
+sys.path.append(os.path.abspath(".."))
+
+project = "Rest API Contacts Manager"
+copyright = "2025, PreArt"
+author = "PreArt"
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+# <https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration>
 
-extensions = []
+extensions = ["sphinx.ext.autodoc"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+# <https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output>
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "nature"
+html_static_path = ["_static"]
